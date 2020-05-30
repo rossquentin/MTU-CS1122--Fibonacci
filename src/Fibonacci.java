f15dda6b0ed70b14b6111e80f1f9c3e6daf0c290
BuildTools <unconfigured@null.spigotmc.org>
2020-05-30 03:32:30 -0400

    Initial commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,21 @@
+public class Fibonacci {
+
+    /**
+     * Generates the nth Fibonacci term recursively
+     *
+     * @param N nth term to calculate to.
+     * @return  nth Fibonacci term.
+     */
+    public static int Fib(int N) {
+        if (N <= 1) {
+            return N;
+        }
+        return Fib(N-1) + Fib(N-2);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Fib(35));
+    }
+}
+
